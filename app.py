63c8b8b3cf68d053dbb37c5f2d7811958d446bd8
Mark Steve Samson <hello@marksteve.com>
2015-12-11 11:26:34 +0800

    Remove html entities

--- app.py
+++ app.py
@@ -58,12 +58,12 @@ Lists environments
 &nbsp;&nbsp;Deploy an environment
 
 *Arguments*
-&nbsp;&nbsp;*environment_id* - ID of environment
+*environment_id* - ID of environment
 
 *Optional arguments*
-&nbsp;&nbsp;*deployed_version* - Version (Git commit) of deployed release, default latest
-&nbsp;&nbsp;*deploy_from_scratch* - Indicates whether the deployment was made from scratch, default false
-&nbsp;&nbsp;*trigger_notifications* - Indicates whether notification should be triggered, default true
+*deployed_version* - Version (Git commit) of deployed release, default latest
+*deploy_from_scratch* - Indicates whether the deployment was made from scratch, default false
+*trigger_notifications* - Indicates whether notification should be triggered, default true
 
 *Example*
 `/deploybot deploy 12345 deployed_version=eibaemauP3seukief6einei6phahpheichais7de deploy_from_scratch=true`
